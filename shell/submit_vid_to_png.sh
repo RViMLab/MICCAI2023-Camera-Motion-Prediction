9af2ed9497eb69d20388933e7361ae61db249f14
mhubii <m.huber_1994@hotmail.de>
2020-10-28 16:25:39 +0000

    removed submissions

--- shell/submit_vid_to_png.sh
+++
@@ -1,9 +0,0 @@
-runai submit hil \
-  -p mhuber \
-  -i 10.202.67.201:32581/mhuber:hil \
-  -v /nfs/home/mhuber/proj/homography_imitation_learning/:/workspace/homography_imitation_learning \
-  -v /nfs/home/mhuber/data:/nfs/home/mhuber/data \
-  -g 0 \
-  --command /workspace/homography_imitation_learning/shell/run_vid_to_png.sh \
-  --working-dir /workspace/homography_imitation_learning/ \
-  --run-as-user # defaults to root -> logs will be safed as root too
