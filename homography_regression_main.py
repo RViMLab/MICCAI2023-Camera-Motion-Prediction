c707d1065737b2838b271b4823c546d06ee9b9f7
mhubii <m.huber_1994@hotmail.de>
2020-11-02 18:09:48 +0000

    added specific homography regression main

---
+++ homography_regression_main.py
@@ -0,0 +1,72 @@
+import os
+import argparse
+import pandas as pd
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+import torch
+
+from utils.io import load_yaml
+import lightning_modules
+from lightning_data_modules import ConsecutiveDataModule
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-sf', '--servers_file', type=str, default='configs/servers.yml', help='Servers file.')
+    parser.add_argument('-s', '--server', type=str, default='local', help='Specify server.')
+    parser.add_argument('-c', '--configs', type=str, required=True, help='Path to configuration file.')
+    args = parser.parse_args()
+
+    servers = load_yaml(args.servers_file)
+    server = servers[args.server]
+
+    configs = load_yaml(args.configs)
+
+    # prepare data
+    prefix = os.path.join(server['database']['location'], configs['data']['pkl_path'])
+    df = pd.read_pickle(os.path.join(
+            prefix,
+            configs['data']['pkl_name']
+    ))
+
+    dm = ConsecutiveDataModule(
+        df, 
+        prefix, 
+        train_split=configs['data']['train_split'],
+        batch_size=configs['data']['batch_size'],
+        num_workers=configs['data']['num_workers'],
+        rho=configs['data']['rho'],
+        crp_shape=configs['data']['crp_shape']
+    )
+    dm.setup()
+
+    # load specific module
+    shape = next(iter(dm.train_dataloader()))['img_seq'][0].shape
+    kwargs = {
+        'shape': [2*shape[1], shape[2], shape[3]],
+        'lr': configs['hparams']['lr'],
+        'betas': configs['hparams']['betas']
+    }
+
+    module = getattr(lightning_modules, configs['lightning_module'])(**kwargs)
+
+    logger = TensorBoardLogger(
+        save_dir=server['logging']['location'],
+        name=configs['experiment']
+    )
+
+    trainer = pl.Trainer(
+        max_epochs=configs['trainer']['max_epochs'],
+        logger=logger,
+        limit_train_batches=configs['trainer']['limit_train_batches'],
+        limit_val_batches=configs['trainer']['limit_val_batches'],
+        limit_test_batches=configs['trainer']['limit_test_batches'],
+        gpus=configs['trainer']['gpus']
+    )
+
+    # fit and validation
+    trainer.fit(module, dm)
+
+    # # test
+    trainer.test()
