73fa89352f6e734a5a3262d715b86f04eca6d94d
mhubii <m.huber_1994@hotmail.de>
2020-11-02 18:17:50 +0000

    added dgx runai shell

---
+++ shell/submit_deep_image_homography_estimation.sh
@@ -0,0 +1,8 @@
+runai submit hil-deep-image-homography-estimation \
+  -p mhuber \
+  -i 10.202.67.201:32581/mhuber:hil \
+  -v /nfs/home/mhuber/proj/homography_imitation_learning/:/workspace/homography_imitation_learning \
+  -g 1 \
+  --command /workspace/homography_imitation_learning/shell/run_deep_image_homography_estimation.sh \
+  --working-dir /workspace/homography_imitation_learning/ \
+  --run-as-user # defaults to root -> logs will be safed as root too
