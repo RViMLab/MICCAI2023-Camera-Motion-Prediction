59462e18f02008c3d9f55514b7ddc413f989a35c
mhubii <m.huber_1994@hotmail.de>
2020-11-02 18:27:43 +0000

    changed env

--- shell/run_deep_image_homography_estimation.sh
+++ shell/run_deep_image_homography_estimation.sh
@@ -3,7 +3,7 @@
 # activate conda environment
 eval "$(conda shell.bash hook)"
 conda env update -f /workspace/homography_imitation_learning/env.yml
-conda activate toy
+conda activate hil
 
 python /workspace/homography_imitation_learning/homography_regression_main.py \
   --server dgx1-1 \
