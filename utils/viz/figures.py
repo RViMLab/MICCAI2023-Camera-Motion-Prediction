699fb89e890494c9c8c550e6c9766f62e1aab371
mhubii <m.huber_1994@hotmail.de>
2020-11-05 17:55:38 +0000

    updated color scale

--- utils/viz/figures.py
+++ utils/viz/figures.py
@@ -8,7 +8,7 @@ def warp_figure(img: np.array, uv: np.array, duv: np.array, duv_pred: np.array, 
     r"""
 
     Args:
-        img (np.array): Original RGB image of shape HxWxC
+        img (np.array): Original RGB image of shape HxWxC [0 ... 1]
         uv (np.array): Crop edges of shape 4x2
         duv (np.array): Deviation from crop edges uv of shape 4x2 
         duv_pred (np.array): Predicted deviation from crop edges uv of shape 4x2
@@ -26,13 +26,13 @@ def warp_figure(img: np.array, uv: np.array, duv: np.array, duv_pred: np.array, 
     wrp = cv2.warpPerspective(img, H, (img.shape[1], img.shape[0]))
 
     # Visualize warped uv, and predicted uv
-    gt_col, pred_col = (0, 255, 255), (255, 255, 0)
+    gt_col, pred_col = (0, 1, 1), (1, 1, 0)
     cv2.polylines(wrp, [wrp_uv[:,::-1].astype(np.int32)], isClosed=True, color=gt_col, thickness=2)
     cv2.polylines(wrp, [wrp_uv_pred[:,::-1].astype(np.int32)], isClosed=True, color=pred_col, thickness=2)
 
     ax = figure.add_subplot(111)
-    gt_patch = mpatches.Patch(color=tuple(t/255 for t in gt_col), label='Ground Truth')
-    pred_patch = mpatches.Patch(color=tuple(t/255 for t in pred_col), label='Prediction')
+    gt_patch = mpatches.Patch(color=gt_col, label='Ground Truth')
+    pred_patch = mpatches.Patch(color=pred_col, label='Prediction')
     ax.legend(handles=[gt_patch, pred_patch])
     plt.imshow(wrp)
 
