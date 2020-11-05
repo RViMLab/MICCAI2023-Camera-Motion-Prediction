a1b435b347205cccd8c574501038b81c9cb822fa
mhubii <m.huber_1994@hotmail.de>
2020-11-05 13:02:12 +0000

    added typing to constitutes

--- models/constitutes.py
+++ models/constitutes.py
@@ -1,13 +1,17 @@
+import torch
 import torch.nn as nn
+from torch.nn.common_types import _size_2_t
+from typing import Callable
 
 class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=0):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: _size_2_t=3, stride: _size_2_t=1, padding: _size_2_t=0, activation: Callable=torch.relu):
         super(ConvBlock, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
         self.norm = nn.BatchNorm2d(num_features=out_channels)
+        self.activation = activation
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
         x = self.conv(x)
         x = self.norm(x)
-        x = torch.relu(x)
+        x = self.activation(x)
         return x
