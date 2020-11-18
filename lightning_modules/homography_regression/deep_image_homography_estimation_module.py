8fcbd26ebfb872e59246d6b043183c653fb50a21
mhubii <m.huber_1994@hotmail.de>
2020-11-18 17:10:59 +0000

    added n steps log

--- lightning_modules/homography_regression/deep_image_homography_estimation_module.py
+++ lightning_modules/homography_regression/deep_image_homography_estimation_module.py
@@ -9,7 +9,7 @@ from utils.viz import warp_figure
 
 
 class DeepImageHomographyEstimationModule(pl.LightningModule):
-    def __init__(self, shape, lr: float=1e-4, betas: List[float]=[0.9, 0.999]):
+    def __init__(self, shape, lr: float=1e-4, betas: List[float]=[0.9, 0.999], log_n_steps: int=1000):
         super().__init__()
         self.save_hyperparameters('lr', 'betas')
         self.model = DeepHomographyRegression(shape)
@@ -17,6 +17,8 @@ class DeepImageHomographyEstimationModule(pl.LightningModule):
 
         self.lr = lr
         self.betas = betas
+        self.validation_step_ct = 0
+        self.log_n_steps = log_n_steps
 
     def forward(self, img, wrp):
         return self.model(img, wrp)
@@ -42,14 +44,16 @@ class DeepImageHomographyEstimationModule(pl.LightningModule):
         ).mean()
         self.log('val/distance', distance_loss)
 
-        figure = warp_figure(
-            img=tensor_to_image(batch['img_seq'][0][0]), 
-            uv=batch['uv'][0].squeeze().numpy(), 
-            duv=batch['duv'][0].squeeze().cpu().numpy(), 
-            duv_pred=duv_pred[0].squeeze().cpu().numpy(), 
-            H=batch['H'][0].squeeze().numpy()
-        )
-        self.logger.experiment.add_figure('val/wrp', figure, self.current_epoch)
+        if self.validation_step_ct % self.log_n_steps == 0:
+            figure = warp_figure(
+                img=tensor_to_image(batch['img_seq'][0][0]), 
+                uv=batch['uv'][0].squeeze().numpy(), 
+                duv=batch['duv'][0].squeeze().cpu().numpy(), 
+                duv_pred=duv_pred[0].squeeze().cpu().numpy(), 
+                H=batch['H'][0].squeeze().numpy()
+            )
+            self.logger.experiment.add_figure('val/wrp', figure, self.current_epoch)
+        self.validation_step_ct += 1
         return distance_loss
 
     def test_step(self, batch, batch_idx):
@@ -58,5 +62,5 @@ class DeepImageHomographyEstimationModule(pl.LightningModule):
             duv_pred.view(-1, 2), 
             batch['duv'].to(duv_pred.dtype).view(-1, 2)
         ).mean()
-        self.log('test/distance', distance_loss)
+        self.log('test/distance', distance_loss, batch_idx)
         return distance_loss
