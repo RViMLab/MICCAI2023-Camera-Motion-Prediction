7389b7bda22f285e640967070f343860ef8f9c4b
mhubii <m.huber_1994@hotmail.de>
2020-11-19 11:43:20 +0000

    added run sh for backboned

---
+++ shell/run_unsupervised_deep_homography_estimation_backbone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# activate conda environment
+eval "$(conda shell.bash hook)"
+conda env update -f /workspace/homography_imitation_learning/env.yml
+conda activate hil
+
+python /workspace/homography_imitation_learning/homography_regression_main.py \
+  --server rvim_server \
+  --configs configs/unsupervised_deep_homography_estimation_backbone.yml
