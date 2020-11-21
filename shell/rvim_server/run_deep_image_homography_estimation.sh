c770dfcda765f18131c930828cc156daafba8d7d
bazinga <bazinga@foo.troll>
2020-11-21 17:40:43 +0000

    added rvim server run scripts

---
+++ shell/rvim_server/run_deep_image_homography_estimation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# activate conda environment
+eval "$(conda shell.bash hook)"
+conda env update -f /home/martin/proj/homography_imitation_learning/env.yml
+conda activate hil
+
+python /home/martin/proj/homography_imitation_learning/homography_regression_main.py \
+  --server rvim_server \
+  --configs configs/deep_image_homography_estimation.yml
