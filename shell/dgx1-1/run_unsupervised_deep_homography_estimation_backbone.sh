832e1b8344d5861b2333b19a058ac78f90f9a6bb
mhubii <m.huber_1994@hotmail.de>
2020-11-21 04:16:40 +0000

    updated dgx runs

--- shell/dgx1-1/run_unsupervised_deep_homography_estimation_backbone.sh
+++ shell/dgx1-1/run_unsupervised_deep_homography_estimation_backbone.sh
@@ -2,7 +2,7 @@
 
 # activate conda environment
 eval "$(conda shell.bash hook)"
-conda env update -f /home/martin/proj/homography_imitation_learning/env.yml
+conda env update -f /workspace/homography_imitation_learning/env.yml
 conda activate hil
 
 python /workspace/homography_imitation_learning/homography_regression_main.py \
