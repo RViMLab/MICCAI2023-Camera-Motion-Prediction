df1cba972438ef01bd6135f863a8472046bb006b
maritn <m.huber_1994@hotmail.de>
2020-11-30 23:35:49 +0100

    updated imitation with feature lstm

--- homography_imitation_main.py
+++ homography_imitation_main.py
@@ -1,1 +1,55 @@
-# place holder
+import os
+import pytorch_lightning as pl
+import pandas as pd
+import argparse
+
+from utils.io import load_yaml
+import lightning_data_modules
+import lightning_modules
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-sf', '--servers_file', type=str, default='configs/servers.yml', help='Servers file.')
+    parser.add_argument('-s', '--server', type=str, default='local', help='Specify server.')
+    parser.add_argument('-c', '--configs', type=str, required=True, help='Path to configuration file.')
+    args = parser.parse_args()
+
+    servers = load_yaml(args.servers_file)
+    server = servers[args.server]
+
+    configs = load_yaml(args.configs)
+
+    # prepare data
+    prefix = os.path.join(server['database']['location'], configs['data']['pkl_path'])
+    df = pd.read_pickle(os.path.join(
+            prefix,
+            configs['data']['pkl_name']
+    ))
+
+    # load specific data module
+    kwargs = {
+        'df': df,
+        'prefix': prefix,
+        'train_split': configs['data']['train_split'],
+        'random_state': configs['data']['random_state'],
+        'batch_size': configs['data']['batch_size'],
+        'num_workers': configs['data']['num_workers'],
+        'train_transforms': configs['data']['train_transforms'],
+        'val_transforms': configs['data']['val_transforms']
+    }
+
+    dm = getattr(lightning_data_modules, configs['lightning_data_module'])
+
+    # load specific module
+    kwargs = configs['model']
+
+    module = getattr(lightning_modules, configs['lightning_module'])
+
+    trainer = pl.Trainer(
+
+    )
+
+    trainer.fit(module, dm)
+
+    # trainer.test()
--- lightning_data_modules/sequence_data_module.py
+++ lightning_data_modules/sequence_data_module.py
@@ -1,17 +1,43 @@
+import pandas as pd
 import pytorch_lightning as pl
+from typing import Callable
+# from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
 
+from datasets import ImageSequenceDataset
+
+
+
+# vid_to_png for single video
+# fit lstm model to single video
+
+# maybe add dataset that read from videos torch.io/opencv/nvidia dataloader/decord?
+# find transforms for all cholec80 videos via processing.ipynb
+# identify test sets -> update cholec80_transforms.yml with transforms and test flag
+# convert whole cholec80 dataset into pngs ~2.6 TB
+# use sequence_dataframe.ipynb to generate sequence dataframe, also include tool and phase annotation 
+# train model on full dataset
+# 
 class SequenceDataModule(pl.LightningDataModule):
-    def __init__(self):
-        pass
+    def __init__(self, df: pd.DataFrame, prefix: str, train_split: float, batch_size: int, num_workers: int=2, train_trainsforms: Callable=None, val_transforms: Callable=None):
+        self.df = df
+        self.prefix = prefix
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+        self.train_transforms = train_trainsforms
 
     def setup(self, stage):
-        pass
+        if stage == 'fit':
+            self.train_set = ImageSequenceDataset(df=self.df, prefix=self.prefix, transforms=self.train_transforms)
+        if stage == 'test':
+            pass
 
     def transfer_batch_to_device(self, batch, device):
         pass
 
     def train_dataloader(self):
-        pass
+        return DataLoader(self.train_set, batch_size=self.batch_size, num_workers=self.num_workers)
 
     def val_dataloader(self):
         pass
