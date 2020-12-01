405d51fe5493ad05369d6ff65a5f55c32a651d49
maritn <m.huber_1994@hotmail.de>
2020-12-01 01:11:11 +0100

    added sorted checkpoints reader

--- homography_imitation_main.py
+++ homography_imitation_main.py
@@ -1,11 +1,13 @@
 import os
-import pytorch_lightning as pl
-import pandas as pd
 import argparse
+import pandas as pd
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
 
-from utils.io import load_yaml
+from utils.io import load_yaml, save_yaml, generate_path, scan2df, natural_keys
 import lightning_data_modules
 import lightning_modules
+
 
 if __name__ == '__main__':
 
@@ -13,12 +15,25 @@ if __name__ == '__main__':
     parser.add_argument('-sf', '--servers_file', type=str, default='configs/servers.yml', help='Servers file.')
     parser.add_argument('-s', '--server', type=str, default='local', help='Specify server.')
     parser.add_argument('-c', '--configs', type=str, required=True, help='Path to configuration file.')
+    parser.add_argument('-bp', '--backbone_path', type=str, required=True, help='Path to log folders, relative to server logging location.')
     args = parser.parse_args()
 
     servers = load_yaml(args.servers_file)
     server = servers[args.server]
 
     configs = load_yaml(args.configs)
+
+    # append configs by backbone
+    backbone_configs = load_yaml(os.path.join(server['logging']['location'], args.backbone_path, 'configs.yml'))
+    df = scan2df(os.path.join(server['logging']['location'], args.backbone_path, 'checkpoints'), '.ckpt')
+    ckpts = sorted(list(df['files']), key=natural_keys)
+    configs['model']['backbone'] = {
+        'lightning_module': backbone_configs['lightning_module'],
+        'model': backbone_configs['model'],
+        'path': args.backbone_path,
+        'checkpoint': 'checkpoints/{}'.format(ckpts[-1]),
+        'experiment': backbone_configs['experiment']
+    }
 
     # prepare data
     prefix = os.path.join(server['database']['location'], configs['data']['pkl_path'])
@@ -46,8 +61,23 @@ if __name__ == '__main__':
 
     module = getattr(lightning_modules, configs['lightning_module'])
 
-    trainer = pl.Trainer(
+    logger = TensorBoardLogger(
+        save_dir=server['logging']['location'],
+        name=configs['experiment']
+    )
 
+    # save configs
+    generate_path(logger.log_dir)
+    save_yaml(os.path.join(logger.log_dir, 'configs.yml'), configs)
+
+    trainer = pl.Trainer(
+        max_epochs=configs['trainer']['max_epochs'],
+        logger=logger,
+        log_every_n_steps=configs['trainer']['log_every_n_steps'],
+        limit_train_batches=configs['trainer']['limit_train_batches'],
+        limit_val_batches=configs['trainer']['limit_val_batches'],
+        limit_test_batches=configs['trainer']['limit_test_batches'],
+        gpus=configs['trainer']['gpus']
     )
 
     trainer.fit(module, dm)
--- utils/io/folders.py
+++ utils/io/folders.py
@@ -1,6 +1,7 @@
 import pathlib
 import os
 import glob
+import re
 
 import pandas as pd
 
@@ -11,14 +12,26 @@ def generate_path(prefix):
 
 def scan2df(folder, postfix='.jpg'):
     # scan folder for images and return dataframe
-    df = pd.DataFrame(columns={'folder', 'file'})
+    df = pd.DataFrame(columns={'folder', 'files'})
 
     for file in glob.glob(os.path.join(folder, '*{}'.format(postfix))):
         dic = {
             'folder': folder,
-            'file': os.path.basename(file)
+            'files': os.path.basename(file)
         }
 
         df = df.append(dic, ignore_index=True)
 
     return df
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    r"""Sorts in human order, see
+        https://stackoverflow.com/questions/5967500/how-to-correctly-sort-a-string-with-a-number-inside
+    
+    Example:
+        sorted_list = list.sort(key=natural_keys)
+    """
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
