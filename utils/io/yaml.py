06446b004fe288ac629ccf26fadcab6c127facde
mhubii <m.huber_1994@hotmail.de>
2021-02-18 09:27:16 +0000

    added pickle io, updated doc strings

--- utils/io/yaml.py
+++ utils/io/yaml.py
@@ -1,16 +1,29 @@
 import yaml
-import os
+from typing import Union
 
 
-def load_yaml(path):
+def load_yaml(path: str) -> Union[dict, bool]:
+    r"""Load YAML file into dictionary.
+
+    Args:
+        path (str): Path to load YAML file from
+    Return:
+        yml (dict): Dictionary representation of YAML file, False if failure
+    """
     try:
-        with open(path, 'r') as stream:
-            yml = yaml.load(stream, Loader=yaml.FullLoader)
+        with open(path, 'r') as f:
+            yml = yaml.load(f, Loader=yaml.FullLoader)
         return yml
     except:
         return False
 
 
-def save_yaml(path, dic):
-    with open(os.path.join(path), "w") as f:
+def save_yaml(path: str, dic: dict) -> None:
+    r"""Save dictionary object into YAML file.
+
+    Args:
+        path (str): Path to write YAML file to, recommended postfix '.yml' or '.yaml'
+        dic (dict): Dictionary to be written to YAML file
+    """
+    with open(path, 'w') as f:
         yaml.dump(dic, f)
