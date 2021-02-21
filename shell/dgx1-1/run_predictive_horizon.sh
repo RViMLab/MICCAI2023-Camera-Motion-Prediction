292f995e02420f0f3f27e344551009f96c2edc68
mhubii <m.huber_1994@hotmail.de>
2021-02-21 20:04:27 +0000

    added dgx scripts

---
+++ shell/dgx1-1/run_predictive_horizon.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# activate conda environment
+eval "$(conda shell.bash hook)"
+conda env update -f /workspace/homography_imitation_learning/env.yml
+conda activate hil
+
+python /workspace/homography_imitation_learning/homography_imitation_main.py \
+  --server dgx1-1 \
+  --configs configs/predictive_horizon.yml \
+  --backbone_path deep_image_homography_estimation_backbone/version_2
