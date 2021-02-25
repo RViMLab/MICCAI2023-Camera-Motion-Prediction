e2243b957a26a2d581bcf77e669184766a8cd757
mhubii <m.huber_1994@hotmail.de>
2021-02-25 18:07:01 +0000

    added half res submissions

---
+++ shell/dgx1-1/submit_deep_image_homography_estimation_backbone_half_res.sh
@@ -0,0 +1,11 @@
+runai submit hil-supervised-backbone-half-res \
+  -p mhuber \
+  -i 10.202.67.201:32581/mhuber:hil_03 \
+  -v /nfs/home/mhuber/proj/homography_imitation_learning/:/workspace/homography_imitation_learning \
+  -v /nfs/home/mhuber/data:/nfs/home/mhuber/data \
+  -v /nfs/home/mhuber/tresorit/homography_imitation_learning_logs:/nfs/home/mhuber/tresorit/homography_imitation_learning_logs \
+  -g 1 \
+  --host-ipc \
+  --command /workspace/homography_imitation_learning/shell/dgx1-1/run_deep_image_homography_estimation_backbone_half_res.sh \
+  --working-dir /workspace/homography_imitation_learning/ \
+  --run-as-user # defaults to root -> logs will be saved as root too
