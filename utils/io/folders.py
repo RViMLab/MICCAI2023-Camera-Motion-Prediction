c0255f65dbd194a7af1bb2929fe11d0843ce07f0
mhubii <m.huber_1994@hotmail.de>
2021-03-05 10:57:51 +0000

    added simple dataframe

--- utils/io/folders.py
+++ utils/io/folders.py
@@ -2,15 +2,16 @@ import pathlib
 import os
 import glob
 import re
+from typing import List, Union
 
 import pandas as pd
 
 
-def generate_path(prefix):
+def generate_path(prefix: str) -> None:
     if not os.path.exists(prefix):
         pathlib.Path(prefix).mkdir(parents=True, exist_ok=True)
 
-def scan2df(folder, postfix='.jpg'):
+def scan2df(folder: str, postfix: str='.jpg') -> pd.DataFrame:
     # scan folder for images and return dataframe
     df = pd.DataFrame(columns={'folder', 'file'})
 
@@ -24,10 +25,24 @@ def scan2df(folder, postfix='.jpg'):
 
     return df
 
-def atoi(text):
+def recursive_scan2df(folder: str, postfix: str='.jpg') -> pd.DataFrame:
+    # scan folder for images and return dataframe
+    df = pd.DataFrame(columns={'folder', 'file'})
+
+    for root, subdirs, files in os.walk(folder):
+        files = [x for x in files if postfix in x]
+        if files:
+            dic_list = [{
+                'folder': root.replace(folder, '').strip('/'), 
+                'file': x
+            } for x in files]
+            df = df.append(dic_list, ignore_index=True)
+    return df
+
+def atoi(text: str) -> Union[int, str]:
     return int(text) if text.isdigit() else text
 
-def natural_keys(text):
+def natural_keys(text: str) -> List[int]:
     r"""Sorts in human order, see
         https://stackoverflow.com/questions/5967500/how-to-correctly-sort-a-string-with-a-number-inside
     
