052ec517da252dc968e27f279f128f261fd20ffd
mhubii <m.huber_1994@hotmail.de>
2021-03-08 14:28:29 +0000

    added increase sub and run scripts

---
+++ shell/dgx1-1/run_deep_image_homography_estimation_backbone_increase.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# activate conda environment
+eval "$(conda shell.bash hook)"
+conda env update -f /workspace/homography_imitation_learning/env_dgx.yml
+conda activate hil
+
+python /workspace/homography_imitation_learning/homography_regression_main.py \
+  --server dgx1-1 \
+  --configs configs/deep_image_homography_estimation_backbone_increase.yml
