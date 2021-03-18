1b31ccdcbf91869c6c55add2994156d3d4cbf2c9
mhubii <m.huber_1994@hotmail.de>
2021-03-18 15:02:03 +0000

    started annotation

--- utils/sampling/consecutive_sequences.py
+++ utils/sampling/consecutive_sequences.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
-from typing import List, Callable
+import torch
+from typing import List, Callable, Union
 
 
 class ConsecutiveSequences():
@@ -15,28 +16,28 @@ class ConsecutiveSequences():
         transforms (list of callables): Transforming videos
         verbose (bool): Return verbose output if true
     """
-    def __init__(self, paths: List[str], stride: int=1, max_seq: int=None, seq_len: int=1, seq_stride: int=None, transforms: Callable=None, verbose: bool=False):
-        self.video_captures = []
-        self.seq = 0
-        self.max_seq = max_seq
-        self.seq_len = seq_len
-        self.vid_idx = 0
-        self.frame_idx = 0
-        self.stride = stride
-        self.seq_stride = seq_stride
+    def __init__(self, paths: List[str], stride: int=1, max_seq: int=None, seq_len: int=1, seq_stride: int=None, transforms: Callable=None, verbose: bool=False) -> None:
+        self._video_captures = []
+        self._seq = 0
+        self._max_seq = max_seq
+        self._seq_len = seq_len
+        self._vid_idx = 0
+        self._frame_idx = 0
+        self._stride = stride
+        self._seq_stride = seq_stride
         if not seq_stride:
-            self.seq_stride = seq_len
-        self.transforms = transforms
-        self.verbose = verbose
-        self.frame_counts = np.empty(len(paths))
+            self._seq_stride = seq_len
+        self._transforms = transforms
+        self._verbose = verbose
+        self._frame_counts = np.empty(len(paths))
         for idx, p in enumerate(paths):
             video_capture = cv2.VideoCapture(p)
-            self.video_captures.append(video_capture)
-            self.frame_counts[idx] = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+            self._video_captures.append(video_capture)
+            self._frame_counts[idx] = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
     def __del__(self):
-        for i in range(len(self.video_captures)):
-            self.video_captures[i].release()
+        for i in range(len(self._video_captures)):
+            self._video_captures[i].release()
 
     def __iter__(self):
         return self
@@ -47,31 +48,31 @@ class ConsecutiveSequences():
         Returns:
             imgs (np.array): Sequence of shape LxHxWxC
 
-            if self.verbose == True:
+            if self._verbose == True:
                 vid_idx (int): Video index, as sorted in paths
                 frame_idx (int): Initial frame index in video
         """
-        if self.vid_idx < len(self.video_captures):
-            if self.transforms:
-                imgs = self._sample(self.video_captures[self.vid_idx], self.frame_idx, self.stride, self.transforms[self.vid_idx])
+        if self._vid_idx < len(self._video_captures):
+            if self._transforms:
+                imgs = self._sample(self._video_captures[self._vid_idx], self._frame_idx, self._stride, self._transforms[self._vid_idx])
             else:
-                imgs = self._sample(self.video_captures[self.vid_idx], self.frame_idx, self.stride)
+                imgs = self._sample(self._video_captures[self._vid_idx], self._frame_idx, self._stride)
 
-            vid_idx = self.vid_idx
-            frame_idx = self.frame_idx
+            vid_idx = self._vid_idx
+            frame_idx = self._frame_idx
 
             # increment counters
-            if self.max_seq is not None:
-                if self.seq >= self.max_seq:
+            if self._max_seq is not None:
+                if self._seq >= self._max_seq:
                     self.__del__()
                     raise StopIteration
-                self.seq += 1
-            self.frame_idx += self.seq_stride
-            if self.frame_counts[self.vid_idx] - self.stride*self.seq_len < self.frame_idx:
-                self.video_captures[self.vid_idx].release()
-                self.frame_idx = 0
-                self.vid_idx += 1
-            if self.verbose:
+                self._seq += 1
+            self._frame_idx += self._seq_stride
+            if self._frame_counts[self._vid_idx] - self._stride*self._seq_len < self._frame_idx:
+                self._video_captures[self._vid_idx].release()
+                self._frame_idx = 0
+                self._vid_idx += 1
+            if self._verbose:
                 return imgs, vid_idx, frame_idx
             else:
                 return imgs
@@ -80,12 +81,12 @@ class ConsecutiveSequences():
         raise StopIteration
 
     def __len__(self):
-        if not self.max_seq:
-            return (self.frame_counts / self.seq_stride + 1).astype(np.int).sum()
+        if not self._max_seq:
+            return (self._frame_counts / self._seq_stride + 1).astype(np.int).sum()
         else:
-            return self.max_seq
+            return self._max_seq
 
-    def _sample(self, capture: cv2.VideoCapture, frame: int, stride: int, transforms: Callable=None):
+    def _sample(self, capture: cv2.VideoCapture, frame: int, stride: int, transforms: Callable=None) -> Union[np.ndarray, torch.Tensor]:
         r"""Return a sequence of images from a videos capture.
 
         Args:
@@ -98,7 +99,7 @@ class ConsecutiveSequences():
             imgs (np.array): Frames to return of shape LxHxWxC
         """
         imgs = []
-        for i in range(self.seq_len):
+        for i in range(self._seq_len):
             capture.set(1, frame+i*stride) # https://stackoverflow.com/questions/33650974/opencv-python-read-specific-frame-using-videocapture
             _, img = capture.read()
             if transforms:
--- utils/sampling/random_sequences.py
+++ utils/sampling/random_sequences.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
-from typing import List, Callable
+import torch
+from typing import List, Callable, Union
 
 
 class RandomSequences():
@@ -14,26 +15,26 @@ class RandomSequences():
         transforms (list of callables): Transforming videos
         verbose (bool): Return verbose output if true
     """
-    def __init__(self, max_seq: int, paths: List[str], seq_len: int=1, strides: List[int]=[1], transforms: Callable=None, verbose: bool=False):
-        self.video_captures = []
-        self.prob_vid = []
-        self.seq = 0
-        self.max_seq = max_seq
-        self.seq_len = seq_len
-        self.strides = strides
-        self.transforms = transforms
-        self.verbose = verbose
+    def __init__(self, max_seq: int, paths: List[str], seq_len: int=1, strides: List[int]=[1], transforms: Callable=None, verbose: bool=False) -> None:
+        self._video_captures = []
+        self._prob_vid = []
+        self._seq = 0
+        self._max_seq = max_seq
+        self._seq_len = seq_len
+        self._strides = strides
+        self._transforms = transforms
+        self._verbose = verbose
         frame_counts = np.empty(len(paths))
         for idx, p in enumerate(paths):
             video_capture = cv2.VideoCapture(p)
-            self.video_captures.append(video_capture)
+            self._video_captures.append(video_capture)
             frame_counts[idx] = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
         for fc in frame_counts:
-            self.prob_vid.append(fc/frame_counts.sum())
+            self._prob_vid.append(fc/frame_counts.sum())
 
     def __del__(self):
-        for i in range(len(self.video_captures)):
-            self.video_captures[i].release()
+        for i in range(len(self._video_captures)):
+            self._video_captures[i].release()
 
     def __iter__(self):
         return self
@@ -44,38 +45,38 @@ class RandomSequences():
         Returns:
             imgs (np.array): Randomly sampled sequence of shape LxHxWxC
 
-            if self.verbose == True:
+            if self._verbose == True:
                 vid_idx (int): Video index, as sorted in paths
                 frame_idx (int): Initial frame index in video
         """
-        if self.seq < self.max_seq:
-            vid_idx = np.random.choice(len(self.video_captures), 1, p=self.prob_vid)
+        if self._seq < self._max_seq:
+            vid_idx = np.random.choice(len(self._video_captures), 1, p=self._prob_vid)
             frame_idx = np.random.randint(
                 0, 
-                self.video_captures[vid_idx[0]].get(cv2.CAP_PROP_FRAME_COUNT) - 1 - max(self.strides)*self.seq_len, 
+                self._video_captures[vid_idx[0]].get(cv2.CAP_PROP_FRAME_COUNT) - 1 - max(self._strides)*self._seq_len, 
                 1
             )
-            stride = np.random.choice(self.strides, 1)[0]
-            self.seq += 1
+            stride = np.random.choice(self._strides, 1)[0]
+            self._seq += 1
             
-            if self.transforms:
-                if self.verbose:
-                    return self._sample(self.video_captures[vid_idx.item(0)], frame_idx.item(0), stride, self.transforms[vid_idx]), vid_idx, frame_idx
+            if self._transforms:
+                if self._verbose:
+                    return self._sample(self._video_captures[vid_idx.item(0)], frame_idx.item(0), stride, self._transforms[vid_idx.item(0)]), vid_idx.item(0), frame_idx.item(0)
                 else:
-                    return self._sample(self.video_captures[vid_idx.item(0)], frame_idx.item(0), stride, self.transforms[vid_idx])
+                    return self._sample(self._video_captures[vid_idx.item(0)], frame_idx.item(0), stride, self._transforms[vid_idx.item(0)])
             else:
-                if self.verbose:
-                    return self._sample(self.video_captures[vid_idx.item(0)], frame_idx.item(0), stride), vid_idx, frame_idx
+                if self._verbose:
+                    return self._sample(self._video_captures[vid_idx.item(0)], frame_idx.item(0), stride), vid_idx.item(0), frame_idx.item(0)
                 else:
-                    return self._sample(self.video_captures[vid_idx.item(0)], frame_idx.item(0), stride)
+                    return self._sample(self._video_captures[vid_idx.item(0)], frame_idx.item(0), stride)
 
         self.__del__()
         raise StopIteration
 
     def __len__(self):
-        return self.max_seq
+        return self._max_seq
 
-    def _sample(self, capture: cv2.VideoCapture, frame: int, stride: int=1, transforms: Callable=None):
+    def _sample(self, capture: cv2.VideoCapture, frame: int, stride: int=1, transforms: Callable=None) -> Union[np.ndarray, torch.Tensor]:
         r"""Return a sequence of images from a videos capture.
 
         Args:
@@ -88,13 +89,13 @@ class RandomSequences():
             imgs (np.array): Frames to return of shape LxHxWxC
         """
         imgs = []
-        for i in range(self.seq_len):
+        for i in range(self._seq_len):
             capture.set(1, frame+i*stride) # https://stackoverflow.com/questions/33650974/opencv-python-read-specific-frame-using-videocapture
             _, img = capture.read()
             if transforms:
                 img = transforms(img)
             imgs.append(img)
-        return np.array(imgs)
+        return imgs
 
 
 if __name__ == '__main__':
