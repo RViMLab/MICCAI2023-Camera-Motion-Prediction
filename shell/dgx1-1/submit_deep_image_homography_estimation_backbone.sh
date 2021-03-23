1d6a14f89d50e13ec8f9e5bcc3c4ee23ccc5919d
mhubii <m.huber_1994@hotmail.de>
2021-03-23 19:20:17 +0000

    changed torch home env variable in container

--- shell/dgx1-1/submit_deep_image_homography_estimation_backbone.sh
+++ shell/dgx1-1/submit_deep_image_homography_estimation_backbone.sh
@@ -4,7 +4,9 @@ runai submit hil-supervised-backbone \
   -v /nfs/home/mhuber/proj/homography_imitation_learning/:/workspace/homography_imitation_learning \
   -v /nfs/home/mhuber/data:/nfs/home/mhuber/data \
   -v /nfs/home/mhuber/tresorit/homography_imitation_learning_logs:/nfs/home/mhuber/tresorit/homography_imitation_learning_logs \
+  -v /nfs/home/mhuber/.torch:/nfs/home/mhuber/.torch \
   -g 1 \
+  --environment TORCH_HOME=/nfs/home/mhuber/.torch \
   --host-ipc \
   --command /workspace/homography_imitation_learning/shell/dgx1-1/run_deep_image_homography_estimation_backbone.sh \
   --working-dir /workspace/homography_imitation_learning/ \
