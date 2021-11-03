11359e60cc546b77b4d7161dbf3bbdc650c9f9f6
mhubii <m.huber_1994@hotmail.de>
2021-11-03 18:10:49 +0000

    added endoscopy view submission files

---
+++ shell/dgx1-1/run_deep_image_homography_estimation_endoscopy_view_backbone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# activate conda environment
+eval "$(conda shell.bash hook)"
+conda env update -f /workspace/homography_imitation_learning/env_dgx.yml
+conda activate torch19
+
+python /workspace/homography_imitation_learning/homography_regression_main.py \
+  --server dgx1-1 \
+  --config config/deep_image_homography_estimation_endoscopy_view_backbone.yml
---
+++ shell/dgx1-1/submit_deep_image_homography_estimation_endoscopy_view_backbone.sh
@@ -0,0 +1,13 @@
+runai submit hil-resnet-34-48-25 \
+  -p mhuber \
+  -i 10.202.67.207:5000/mhuber:torch19 \
+  -v /nfs/home/mhuber/proj/homography_imitation_learning/:/workspace/homography_imitation_learning \
+  -v /nfs/home/mhuber/data:/nfs/home/mhuber/data \
+  -v /nfs/home/mhuber/tresorit/homography_imitation_learning_logs:/nfs/home/mhuber/tresorit/homography_imitation_learning_logs \
+  -v /nfs/home/mhuber/.torch:/nfs/home/mhuber/.torch \
+  -g 1 \
+  --environment TORCH_HOME=/nfs/home/mhuber/.torch \
+  --large-shm \
+  --command /workspace/homography_imitation_learning/shell/dgx1-1/run_deep_image_homography_estimation_endoscopy_view_backbone.sh \
+  --working-dir /workspace/homography_imitation_learning/ \
+  --run-as-user # defaults to root -> logs will be saved as root too
