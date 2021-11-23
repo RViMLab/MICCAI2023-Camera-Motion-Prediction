3f18dc20ad52ddb4afce9a39427a4a70347492d9
mhubii <m.huber_1994@hotmail.de>
2021-11-23 17:46:28 +0000

    added nth frame processing

--- shell/dgx1-1/run_pre_processing.sh
+++ shell/dgx1-1/run_pre_processing.sh
@@ -10,6 +10,7 @@ python /workspace/homography_imitation_learning/pre_processing_main.py \
   --backbone_path endoscopy_view/resnet/34/version_0 \
   --data_prefix cholec80_frames \
   --in_pkl log.pkl \
-  --out_pkl pre_processed_log.pkl \
+  --out_pkl pre_processed_5th_frame_log.pkl \
   --num_workers 20 \
-  --batch_size 400
+  --batch_size 400 \
+  --nth_frame 5
