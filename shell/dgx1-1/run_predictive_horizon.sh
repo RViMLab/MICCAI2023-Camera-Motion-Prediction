796ca0f808a168efe28d512355d500551e1e5b09
mhubii <m.huber_1994@hotmail.de>
2021-11-28 15:50:32 +0000

    updated env updates

--- shell/dgx1-1/run_predictive_horizon.sh
+++ shell/dgx1-1/run_predictive_horizon.sh
@@ -2,10 +2,10 @@
 
 # activate conda environment
 eval "$(conda shell.bash hook)"
-conda env update -f /workspace/homography_imitation_learning/env_dgx.yml
+conda env update -f /workspace/homography_imitation_learning/env_110.yml
 conda activate torch110
 
-python /workspace/homography_imitation_learning/homography_imitation_main.py \
+python /workspace/homography_imitation_learning/homography_imitation_seq_main.py \
   --server dgx1-1 \
-  --config config/predictive_horizon_dgx.yml \
-  --backbone_path endoscopy_view/resnet/34/version_0
+  --config config/predictive_horizon_seq_dgx.yml \
+  --backbone_path ae_cai/resnet/48/25/34/version_0
