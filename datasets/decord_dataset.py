189937a73301b35dba72a1b8d90cc5d919cc0ab6
mhubii <m.huber_1994@hotmail.de>
2021-12-06 14:56:32 +0000

    switched to videoloader decord

--- datasets/decord_dataset.py
+++ datasets/decord_dataset.py
@@ -1,59 +1,42 @@
 import os
 import random
-import torch
 import pandas as pd
 import numpy as np
-from decord import VideoReader
-from typing import Callable
+from decord import VideoLoader, cpu, bridge
+from decord._ffi.ndarray import DECORDContext
+from typing import Callable, Tuple
 from torch.utils.data import Dataset
 
 
 class DecordDataset(Dataset):
     def __init__(self,
-        data_df: list,
+        data_df: pd.DataFrame,
         prefix: str,
-        seq_len: int = 1,
-        frame_increment: int=1,
-        frames_between_clips: int=1,
-        seeds: bool=True,
-        transforms: Callable = None,
-        num_threads: int = 0
+        seeds: bool=False,
+        transforms: Callable=None,
+        ctx: DECORDContext=cpu(0),
+        shape: Tuple[int]=(1,240,320,3),  # BxHxWxC
+        interval: int=0,
+        skip: int=0,
+        shuffle: int=0
     ):
+        bridge.set_bridge("torch")
         self._data_df = data_df.sort_values(["folder", "file"]).reset_index(drop=True)
-        self._seq_len = seq_len
-        self._frame_increment = frame_increment
-        self._frames_between_clips = frames_between_clips
         self._transforms = transforms
         if self._transforms:
             raise NotImplemented("DecordDataset: No transforms implemented.")
         self._seeds = seeds
 
-        # Create video reader for each file
-        self.video_readers = []
-        self._vid_df = pd.DataFrame(columns=["vid", "frame"])
-        for idx, row in self._data_df.iterrows():
-            absolut_path = os.path.join(prefix, row.folder, row.file)
-            self.video_readers.append(VideoReader(
-                uri=absolut_path,
-                num_threads=num_threads
-            ))       
-            
-            frame_count = len(self.video_readers[idx])
+        # Create video loader
+        self._paths = [os.path.join(prefix, row.folder, row.file) for _, row in self._data_df.iterrows()]
 
-            vid_df = pd.DataFrame(columns=["vid", "frame"])
-            vid_df["vid"] = [idx]*frame_count
-            vid_df["frame"] = [i for i in range(frame_count)]
-            self._vid_df = self._vid_df.append(
-                vid_df, ignore_index=True
-            )
-
-        self._vid_df = self._vid_df.sort_values(["vid", "frame"]).reset_index(drop=True)
-
-        self._idcs = self._filterFeasibleSequenceIndices(
-            self._vid_df, col="vid",
-            seq_len=self._seq_len,
-            frame_increment=self._frame_increment,
-            frames_between_clips=self._frames_between_clips
+        self._video_loader = VideoLoader(
+            uris=self._paths,
+            ctx=ctx,
+            shape=shape,
+            interval=interval,
+            skip=skip,
+            shuffle=shuffle
         )
 
     def __getitem__(self, idx):
@@ -62,34 +45,14 @@ class DecordDataset(Dataset):
         else:
             seed = idx
 
-        frame_idcs = self._vid_df.loc[self._idcs[idx]].frame + np.arange(self._seq_len)*self._frame_increment
-        vid_idx = self._vid_df.loc[self._idcs[idx]].vid
+        frames, idcs = next(self._video_loader)
 
-        # sample a sequence in video
-        sequence = self.video_readers[vid_idx].get_batch(
-            frame_idcs
-        ).asnumpy()
+        # # TODO: add transforms
 
-        # TODO: add transforms
-
-        return sequence, frame_idcs, torch.full(frame_idcs.shape, vid_idx)
+        return frames, idcs
 
     def __len__(self):
-        return len(self._idcs)
-
-    def _filterFeasibleSequenceIndices(self, 
-        df: pd.DataFrame,
-        col: str="vid",
-        seq_len: int=2,
-        frame_increment: int=1,
-        frames_between_clips: int=1
-
-    ) -> pd.DataFrame:
-        grouped_df = df.groupby(col)
-        return grouped_df.apply(
-            lambda x: x.iloc[:len(x) - (seq_len - 1)*frame_increment:frames_between_clips]  # get indices [0, length - (seq_len - 1)]
-        ).index.get_level_values(1)  # return 2nd values of pd.MultiIndex
-
+        return len(self._video_loader)
 
 if __name__ == "__main__":
     import torch
@@ -101,6 +64,7 @@ if __name__ == "__main__":
 
     prefix = "/media/martin/Samsung_T5/data/endoscopic_data/cholec80/sample_videos"
     data_df = recursive_scan2df(prefix, ".mp4")
+    shape = [100, 480, 640, 3]
 
     device = "cpu"
     if torch.cuda.is_available():
@@ -111,32 +75,33 @@ if __name__ == "__main__":
     ds = DecordDataset(
         data_df=data_df,
         prefix=prefix,
-        seq_len=100,
-        frame_increment=1,
-        frames_between_clips=150,
-        num_threads=1
+        shape=shape
     )
-    dl = DataLoader(ds, batch_size=1, num_workers=0)
+    dl = DataLoader(ds, batch_size=1, num_workers=0)  # num_workers must be 0
     log_df = pd.DataFrame(columns=["vid", "frame", "center", "radius"])
 
     for batch in tqdm(dl):
-        imgs, frame_idcs, vid_idcs = batch
-        imgs = imgs[0].to(device).float().permute(0, 3, 1, 2)/255.
-        center, radius = detector(imgs, reduction="max")
+        frames, idcs = batch
+        frames, idcs = frames[0], idcs[0]
+
+        frames = frames.to(device).float().permute(0, 3, 1, 2)/255.
+        center, radius = detector(frames, reduction="max")
         # box = endoscopy.max_rectangle_in_circle(imgs.shape, center, radius)
         # crops = kornia.geometry.crop_and_resize(imgs, box, [240, 320])
 
         seq_data = {
-            "vid": vid_idcs[0].numpy().tolist(),
-            "frame": frame_idcs[0].numpy().tolist(),
-            "center": center.cpu().tolist()*len(frame_idcs[0]),
-            "radius": radius.cpu().tolist()*len(frame_idcs[0])
+            "vid": idcs[:,0].numpy().tolist(),
+            "frame": idcs[:,1].numpy().tolist(),
+            "shape": [shape]*len(idcs[:,1]),
+            "center": center.cpu().tolist()*len(idcs[:,1]),
+            "radius": radius.cpu().tolist()*len(idcs[:,1])
         }
 
         seq_df = pd.DataFrame(seq_data)
         log_df = log_df.append(
             seq_df, ignore_index=True
         )
+        print(log_df)
 
     log_df.to_pickle("/media/martin/Samsung_T5/data/endoscopic_data/cholec80_circle_tracking/circle_log.pkl")
     log_df.to_csv("/media/martin/Samsung_T5/data/endoscopic_data/cholec80_circle_tracking/circle_log.csv")
--- vid_to_frames_decord.py
+++ vid_to_frames_decord.py
@@ -2,7 +2,9 @@ import os
 import argparse
 import torch
 import pandas as pd
+from torch.utils import data
 from tqdm import tqdm
+from decord import VideoLoader, cpu, bridge
 
 import endoscopy
 from datasets import DecordDataset
@@ -14,10 +16,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-sf", "--servers_file", type=str, default="config/servers.yml", help="Servers file.")
     parser.add_argument("-s", "--server", type=str, default="local", help="Specify server.")
-    parser.add_argument("-rf", "--recursive_folder", type=str, default="cholec80/sample_videos", help="Folder to be recursively searched, relative to server['database']['location'].")
+    parser.add_argument("-rf", "--recursive_folder", type=str, default="cholec80/videos", help="Folder to be recursively searched, relative to server['database']['location'].")
     parser.add_argument("-of", "--output_folder", type=str, default="cholec80_circle_tracking", help="Output folder, relative to server['database']['location'].")
     parser.add_argument("--seq_len", type=int, default=100, help="Set sequence length used for segmentation reduction.")
-    parser.add_argument("--num_threads", type=int, default=5, help="Decord threads for loading frames from video.")
+    parser.add_argument("--num_threads", type=int, default=8, help="Decord threads for loading frames from video.")
+    parser.add_argument("--shape", nargs="+", default=[100, 320, 640, 3], help="Reshaped image shape.")
     args = parser.parse_args()
 
     server = args.server
@@ -25,6 +28,7 @@ if __name__ == "__main__":
     prefix = os.path.join(server["database"]["location"], args.recursive_folder)
 
     data_df = recursive_scan2df(prefix, ".mp4")
+    data_df = data_df.sort_values(["folder", "file"]).reset_index(drop=True)
 
     device = "cpu"
     if torch.cuda.is_available():
@@ -32,27 +36,31 @@ if __name__ == "__main__":
 
     detector = endoscopy.BoundingCircleDetector(device=device)
 
-    ds = DecordDataset(
-        data_df=data_df,
-        prefix=prefix,
-        seq_len=args.seq_len,
-        frame_increment=1,
-        frames_between_clips=args.seq_len,
-        num_threads=args.num_threads
+    # Create video loader
+    paths = [os.path.join(prefix, row.folder, row.file) for _, row in data_df.iterrows()]   
+    bridge.set_bridge("torch")
+    dl = VideoLoader(
+        uris=paths,
+        ctx=cpu(0),
+        shape=args.shape,
+        interval=0,
+        skip=0,
+        shuffle=0
     )
-    dl = DataLoader(ds, batch_size=1, num_workers=0)
-    log_df = pd.DataFrame(columns=["vid", "frame", "center", "radius"])
+    log_df = pd.DataFrame(columns=["vid", "frame", "center", "radius", "shape"])
 
     for batch in tqdm(dl):
-        imgs, frame_idcs, vid_idcs = batch
-        imgs = imgs[0].to(device).float().permute(0, 3, 1, 2)/255.
+        imgs, idcs = batch
+        vid_idcs, frame_idcs = idcs[:,0], idcs[:,1]
+        imgs = imgs.to(device).float().permute(0, 3, 1, 2)/255.
         center, radius = detector(imgs, reduction="max")
 
         seq_data = {
             "vid": vid_idcs[0].numpy().tolist(),
             "frame": frame_idcs[0].numpy().tolist(),
-            "center": center.cpu().tolist()*len(frame_idcs[0]),
-            "radius": radius.cpu().tolist()*len(frame_idcs[0])
+            "center": center.cpu().tolist()*len(frame_idcs),
+            "radius": radius.cpu().tolist()*len(frame_idcs),
+            "shape": [args.shape]*len(frame_idcs)
         }
 
         seq_df = pd.DataFrame(seq_data)
