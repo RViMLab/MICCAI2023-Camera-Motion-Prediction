96ade393bb50733cd92bc9bf8e5baac612ce965a
mhubii <m.huber_1994@hotmail.de>
2022-01-26 12:14:38 +0000

    removed backbone

--- shell/dgx1-1/run_lstm.sh
+++ shell/dgx1-1/run_lstm.sh
@@ -6,5 +6,4 @@ conda activate torch110
 
 python /workspace/homography_imitation_learning/homography_imitation_seq_main.py \
   --server dgx1-1 \
-  --config config/duv_lstm.yml \
-  --backbone_path ae_cai/resnet/48/25/34/version_0
+  --config config/duv_lstm.yml
