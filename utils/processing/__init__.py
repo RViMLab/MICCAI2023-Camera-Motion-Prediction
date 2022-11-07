64ca5669325a5e4e10b56597cf8abbc93fdb4a0c
mhubii <m.huber_1994@hotmail.de>
2022-11-07 20:08:05 +0100

    added loftr

--- utils/processing/__init__.py
+++ utils/processing/__init__.py
@@ -1,5 +1,5 @@
 from .endoscopy import endoscopy
-from .feature_homography import FeatureHomographyEstimation
+from .feature_homography import FeatureHomographyEstimation, LoFTRHomographyEstimation
 from .helpers import *
 from .random_edge_homography import RandomEdgeHomography
 from .video_sequencing import MultiProcessVideoSequencer, MultiProcessVideoSequencerPlusCircleCropping, SingleProcessInferenceVideoSequencer
--- utils/processing/feature_homography.py
+++ utils/processing/feature_homography.py
@@ -1,7 +1,11 @@
+from typing import List, Tuple, Union
 
 import cv2
+import kornia
 import numpy as np
-from typing import Union, Tuple, List
+import torch
+
+from helpers import image_edges
 
 
 class FeatureHomographyEstimation(object):
@@ -19,8 +23,8 @@ class FeatureHomographyEstimation(object):
         wrp_est = cv2.warpPerspective(img, H, (img.shape[1], img.shape[0]))
     """
     def __init__(self, fd: cv2.Feature2D) -> None:
-        self.fd = fd
-        self.matcher = cv2.FlannBasedMatcher()
+        self._fd = fd
+        self._matcher = cv2.FlannBasedMatcher()
 
     def __call__(self, img: np.array, wrp: np.array, ransacReprojThreshold=5.0, return_kp: bool=False) -> Union[Tuple[np.array, np.array], Tuple[np.array, np.array, np.array, np.array, np.array]]:
         r"""Estimates the homography between images and returns point representation and homography.
@@ -42,8 +46,8 @@ class FeatureHomographyEstimation(object):
         uv = self._image_edges(img)
 
         img, wrp = self._grayscale(img, wrp)
-        kp_img, des_img = self.fd.detectAndCompute(img, None)
-        kp_wrp, des_wrp = self.fd.detectAndCompute(wrp, None)
+        kp_img, des_img = self._fd.detectAndCompute(img, None)
+        kp_wrp, des_wrp = self._fd.detectAndCompute(wrp, None)
         if des_img is None or des_wrp is None or des_img.shape[0] < 2 or des_wrp.shape[0] < 2:
             if return_kp:
                 return None, None, None, None, None
@@ -81,7 +85,7 @@ class FeatureHomographyEstimation(object):
         return img, wrp
 
     def _match(self, queryDescriptors, trainDescriptors) -> List[cv2.DMatch]:
-        matches = self.matcher.knnMatch(queryDescriptors, trainDescriptors, k=2)
+        matches = self._matcher.knnMatch(queryDescriptors, trainDescriptors, k=2)
 
         good_matches = []
         for m,n in matches:
@@ -110,31 +114,107 @@ class FeatureHomographyEstimation(object):
         )
         return uv
 
+class LoFTRHomographyEstimation(object):
+    def __init__(self) -> None:
+        self._loftr = kornia.feature.LoFTR()
+        self._ransac = kornia.geometry.RANSAC(inl_th=1.0)    
 
-if __name__ == '__main__':
-    fd = cv2.SIFT_create()
-    fh = FeatureHomographyEstimation(fd)
+    def __call__(self, img: torch.Tensor, wrp: torch.Tensor) -> torch.Tensor:
+        input = {"image0": kornia.color.rgb_to_grayscale(img), "image1": kornia.color.rgb_to_grayscale(wrp)}
+        correspondence_dict = self._loftr(input)
 
-    img = np.load('utils/processing/sample.npy')
+        H = []
+        for i in range(img.shape[0]): # ransac might not be parallelizeable!
+            try:
+                H.append(self._ransac(correspondence_dict["keypoints0"][correspondence_dict["batch_indexes"] == i], correspondence_dict["keypoints1"][correspondence_dict["batch_indexes"] == i])[0])
+            except:
+                H.append(None)
+        H = torch.stack(H)
+        uv0 = image_edges(img).flip(-1)
+        uv1 = kornia.geometry.transform_points(H, uv0)
+        return (uv0 - uv1).flip(-1)  # OpenCV convention, this really needs a fix, includes training of deep homography estimation!
 
-    # create fake warp
-    H = np.eye(3)
-    H[0, 0] += 1
-    wrp = cv2.warpPerspective(img, H, (img.shape[1], img.shape[0]))
+    def to(self, device: torch.device) -> None:
+        self._loftr.to(device=device)
+        self._ransac.to(device=device)
 
-    # estimate warp
-    H_pred, duv = fh(img, wrp)
-    wrp_est = cv2.warpPerspective(img, H, (img.shape[1], img.shape[0]))
 
-    # plot results
-    shape = (int(img.shape[1]/2.), int(img.shape[0]/2.))
+if __name__ == "__main__":
+    def test_cv_homography():
+        fd = cv2.SIFT_create()
+        fh = FeatureHomographyEstimation(fd)
 
-    img = cv2.resize(img, shape)
-    wrp = cv2.resize(wrp, shape)
-    wrp_est = cv2.resize(wrp_est, shape)
+        img = np.load("utils/processing/sample.npy")
 
-    cv2.imshow('composite', np.concatenate([img, wrp ,wrp_est], axis=1))
-    cv2.waitKey()
+        # create fake warp
+        H = np.eye(3)
+        H[0, 0] += 1
+        wrp = cv2.warpPerspective(img, H, (img.shape[1], img.shape[0]))
 
-    print('H - H_pred:\n', H - H_pred)
-    print('duv:\n', duv)
+        # estimate warp
+        H_pred, duv = fh(img, wrp)
+        wrp_est = cv2.warpPerspective(img, H, (img.shape[1], img.shape[0]))
+
+        # plot results
+        shape = (int(img.shape[1]/2.), int(img.shape[0]/2.))
+
+        img = cv2.resize(img, shape)
+        wrp = cv2.resize(wrp, shape)
+        wrp_est = cv2.resize(wrp_est, shape)
+
+        cv2.imshow("composite", np.concatenate([img, wrp ,wrp_est], axis=1))
+        cv2.waitKey()
+
+        print("H - H_pred:\n", H - H_pred)
+        print("duv:\n", duv)
+    
+    def test_kornia_homography():
+        import cv2
+        import matplotlib.pyplot as plt
+        from kornia import image_to_tensor, tensor_to_image
+        from kornia.geometry import warp_perspective
+        from helpers import four_point_homography_to_matrix, image_edges
+
+        estimator = LoFTRHomographyEstimation()
+        device = "cpu"
+        if torch.cuda.is_available():
+            device = "cuda"
+        estimator.to(device)
+
+        img = np.load("utils/processing/sample.npy")[:,:,::-1]
+        img = cv2.resize(img, [320, 240])
+
+        # create fake warp
+        H = np.eye(3)
+        H[0, 0] += 1
+        wrps = []
+        wrps.append(cv2.warpPerspective(img, H, (img.shape[1], img.shape[0])))
+        H[0, 1] += 1
+        wrps.append(cv2.warpPerspective(img, H, (img.shape[1], img.shape[0])))
+
+        plt.imshow(wrps[0])
+        plt.show()
+        plt.imshow(wrps[1])
+        plt.show()
+
+        # convert to torch
+        img = image_to_tensor(img, False)
+        for i in range(len(wrps)):
+            wrps[i] = image_to_tensor(wrps[i], True)
+        imgs = torch.concat([img, img])
+        wrps = torch.stack(wrps)
+        imgs, wrps = imgs.to(device).float()/255., wrps.to(device).float()/255.
+
+        # estimate warp
+        duvs = estimator(imgs, wrps)
+
+        hs = four_point_homography_to_matrix(image_edges(imgs), -duvs)
+        wrps_pred = warp_perspective(imgs, hs, imgs.shape[-2:])
+        for wrp_pred in wrps_pred:
+            wrp_pred = tensor_to_image(wrp_pred, False)
+            plt.imshow(wrp_pred)
+            plt.show()
+        
+    # test_cv_homography()
+    test_kornia_homography()
+
