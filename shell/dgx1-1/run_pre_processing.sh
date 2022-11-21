df75eb73ee0200acdc9d253de2c7ad5b377f24ef
mhubii <m.huber_1994@hotmail.de>
2022-11-21 18:19:16 +0100

    updated

--- shell/dgx1-1/run_pre_processing.sh
+++ shell/dgx1-1/run_pre_processing.sh
@@ -4,7 +4,7 @@
 eval "$(conda shell.bash hook)"
 conda activate torch110
 
-python /workspace/homography_imitation_learning/pre_processing_main.py \
+python /workspace/homography_imitation_learning/main_pre_processing.py \
   --server dgx1-1 \
   --backbone_path ae_cai/resnet/48/25/34/version_0 \
   --data_prefix cholec80_single_video_frames_cropped \
