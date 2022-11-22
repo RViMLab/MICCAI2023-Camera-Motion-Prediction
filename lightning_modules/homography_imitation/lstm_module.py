31ee389a246f0b5c2a986fb03e924e0180cb2d89
mhubii <m.huber_1994@hotmail.de>
2022-11-22 11:04:49 +0100

    re-sorted indices

--- lightning_modules/homography_imitation/lstm_module.py
+++ lightning_modules/homography_imitation/lstm_module.py
@@ -306,77 +306,52 @@ class FeatureLSTMModule(pl.LightningModule):
             loss["name"]
         )(**loss["kwargs"])
 
-        self._homography_regression = None
         self._val_logged = False
         self._frame_stride = frame_stride
 
     def inject_homography_regression(self, homography_regression: dict, homography_regression_prefix: str):
-        # load trained homography regression model
-        self._homography_regression = getattr(lightning_modules, homography_regression['lightning_module']).load_from_checkpoint(
-            checkpoint_path=os.path.join(homography_regression_prefix, homography_regression['path'], homography_regression['checkpoint']),
-            **homography_regression['model']
-        )
-        self._homography_regression = self._homography_regression.eval()
-        self._homography_regression.freeze()
+        raise RuntimeError("Currently not supported.")
 
-    def on_train_epoch_start(self) -> None:
-        if self._homography_regression:
-            self._homography_regression = self._homography_regression.eval()
-            self._homography_regression.freeze()
-
-    def forward(self, imgs: torch.Tensor, duvs_i: torch.Tensor) -> Any: #, duvs: torch.Tensor) -> Any:
-        B, T, C, H, W = imgs.shape
+    def forward(self, imgs_ip1: torch.Tensor, duvs_i: torch.Tensor, dduvs_im1: torch.Tensor) -> torch.Tensor:
+        # for indices, see https://drive.google.com/file/d/1T1HV01G0bzM_xAavhefhGgsOHGRQmCGw/view?usp=share_link
+        B, T, C, H, W = imgs_ip1.shape
 
         # forward videos into latent space
-        imgs = imgs.reshape(-1, C, H, W) # BxTxCxHxW -> B*TxCxHxW
-        features = self._encoder(imgs)
-        features = features.view(B, T, -1) # B*TxF -> BxTxF, where F = features
-        features = features.permute(1, 0, 2) # BxTxF -> TxBxF
+        imgs_ip1 = imgs_ip1.reshape(-1, C, H, W) # BxTxCxHxW -> B*TxCxHxW
+        f_ip1 = self._encoder(imgs_ip1)
+        f_ip1 = f_ip1.view(B, T, -1) # B*TxF -> BxTxF, where F = features
+        f_ip1 = f_ip1.permute(1, 0, 2) # BxTxF -> TxBxF
 
-        # differentiate
-        duvs_i = duvs_i.permute(1,0,2,3) # BxTx4x2 -> TxBx4x2
-        dduvs_i = differentiate_duv(duvs_i, False)
-        dduvs_i = dduvs_i.view(T-1, B, -1) # (T-1)xBx4x2 -> (T-1)xBx8
+        # prepare dduv
+        dduvs_im1 = dduvs_im1.view(B, T, -1) # BxTx4x2 -> BxTx8
+        dduvs_im1 = dduvs_im1.permute(1,0,2) # BxTx8 -> TxBx8
 
         # lstm and head
-        features = torch.concat([features[1:], dduvs_i], axis=-1)
-        dduvs_ip1, (hn, cn) = self._lstm(features)
-        dduvs_ip1 = self._head(dduvs_ip1)
+        f_ip1 = torch.concat([f_ip1, dduvs_im1], axis=-1)
+        dduvs_i, (hn, cn) = self._lstm(f_ip1)
+        dduvs_i = self._head(dduvs_i)
 
+        dduvs_i = dduvs_i.view(T, B, 4, 2) # TxBx8 -> TxBx4x2
+        dduvs_i = dduvs_i.permute(1,0,2,3) # TxBx4x2 -> BxTx4x2
+        duvs_ip1 = duvs_i + dduvs_i
 
-        dduvs_ip1 = dduvs_ip1.view(dduvs_ip1.shape[:2] + (4, 2,)) # (T-1)xBx8 -> (T-1)xBx4x2
-        dduvs_ip1 = dduvs_ip1.permute(1,0,2,3) # (T-1)xBx4x2 -> Bx(T-1)x4x2
-        duvs_i = duvs_i.permute(1,0,2,3) # TxBx4x2 -> BxTx4x2
-        duvs_ip2 = duvs_i[:,1:] + dduvs_ip1 # duvs_ip2 = duvs_ip1 + dduvs_ip1
-
-        return duvs_ip2
+        return duvs_ip1
 
     def configure_optimizers(self) -> Any:
         return self._optimizer
 
     def training_step(self, batch, batch_idx) -> STEP_OUTPUT:
-        if self._homography_regression: # estimate homography online
-            imgs, tf_imgs, frame_idcs, vid_idcs = batch# images and transformed images
-            imgs, tf_imgs = imgs.float()/255., tf_imgs.float()/255.
-            frames_i, frames_ips = frame_pairs(imgs, self._frame_stride)  # re-sort images
-            frames_i   = frames_i.reshape((-1,) + frames_i.shape[-3:])      # reshape BxNxCxHxW -> B*NxCxHxW
-            frames_ips = frames_ips.reshape((-1,) + frames_ips.shape[-3:])  # reshape BxNxCxHxW -> B*NxCxHxW
-
-            with torch.no_grad():
-                duvs_reg = self._homography_regression(frames_i, frames_ips)
-                duvs_reg = duvs_reg.view(imgs.shape[0], -1, 4, 2) # BxTx4x2
-            tf_imgs = tf_imgs[:,:-1] # skip last image in sequence  
-        else: # load homography offline
-            tf_imgs, duvs_reg, frame_idcs, vid_idcs = batch # transformed images and four point homography
-            tf_imgs = tf_imgs.float()/255.
-            duvs_reg = duvs_reg.float()
+        tf_imgs, duvs_reg, frame_idcs, vid_idcs = batch # transformed images and four point homography
+        tf_imgs = tf_imgs.float()/255.
+        duvs_reg = duvs_reg.float()
 
         # forward model
-        duvs_ip2 = self(tf_imgs, duvs_reg)
+        dduvs_reg = differentiate_duv(duvs_reg, True) # Bx(T-1)x4x2
+        duvs_ip1 = self(tf_imgs[:,2:], duvs_reg[:,1:-1], dduvs_reg[:,:-1])
 
         # compute loss
         loss = self._loss(
-            duvs_ip2[:,:-1].reshape(-1, 2), # we don't have ground truth for the last value in the sequence
+            duvs_ip1.reshape(-1, 2), # we don't have ground truth for the last value in the sequence
             duvs_reg[:,2:].reshape(-1, 2) # note that the first two values are skipped
         )
 
@@ -384,32 +359,21 @@ class FeatureLSTMModule(pl.LightningModule):
 
         return {
             "loss": loss.mean(),
-            'per_sequence_loss': loss.detach().view(duvs_ip2.shape[0], -1).mean(axis=-1).cpu().numpy()
+            'per_sequence_loss': loss.detach().view(duvs_ip1.shape[0], -1).mean(axis=-1).cpu().numpy()
         }
 
     def validation_step(self, batch, batch_idx) -> Optional[STEP_OUTPUT]:
-        # by default without grad (torch.set_grad_enabled(False))
-        if self._homography_regression: # estimate homography online
-            imgs, tf_imgs, frame_idcs, vid_idcs = batch # images and transformed images
-            imgs, tf_imgs = imgs.float()/255., tf_imgs.float()/255.
-            frames_i, frames_ips = frame_pairs(imgs, self._frame_stride)  # re-sort images
-            frames_i   = frames_i.reshape((-1,) + frames_i.shape[-3:])      # reshape BxNxCxHxW -> B*NxCxHxW
-            frames_ips = frames_ips.reshape((-1,) + frames_ips.shape[-3:])  # reshape BxNxCxHxW -> B*NxCxHxW
-
-            duvs_reg = self._homography_regression(frames_i, frames_ips)
-            duvs_reg = duvs_reg.view(imgs.shape[0], -1, 4, 2) # Bx(T-1)x4x2
-            tf_imgs = tf_imgs[:,:-1] # skip last image in sequence  
-        else: # load homography offline
-            tf_imgs, duvs_reg, frame_idcs, vid_idcs = batch # transformed images and four point homography
-            tf_imgs = tf_imgs.float()/255.
-            duvs_reg = duvs_reg.float()
+        tf_imgs, duvs_reg, frame_idcs, vid_idcs = batch # transformed images and four point homography
+        tf_imgs = tf_imgs.float()/255.
+        duvs_reg = duvs_reg.float()
 
         # forward model
-        duvs_ip2 = self(tf_imgs, duvs_reg)
+        dduvs_reg = differentiate_duv(duvs_reg, True) # Bx(T-1)x4x2
+        duvs_ip1 = self(tf_imgs[:,2:], duvs_reg[:,1:-1], dduvs_reg[:,:-1])
 
         # compute loss
         loss = self._loss(
-            duvs_ip2[:,:-1].reshape(-1, 2), # we don't have ground truth for the last value in the sequence
+            duvs_ip1.reshape(-1, 2), # we don't have ground truth for the last value in the sequence
             duvs_reg[:,2:].reshape(-1, 2) # note that the first two values are skipped
         )
 
@@ -426,7 +390,7 @@ class FeatureLSTMModule(pl.LightningModule):
 
             uv = image_edges(frames_i[0,0].unsqueeze(0))
             uv_reg = integrate_duv(uv, duvs_reg[0,1:])  # batch 0, note that first value is skipped
-            uv_pred = integrate_duv(uv, duvs_ip2[0])  # batch 0
+            uv_pred = integrate_duv(uv, duvs_ip1[0])  # batch 0
             uv_traj_fig = uv_trajectory_figure(uv_reg.cpu().numpy(), uv_pred.detach().cpu().numpy())
             self.logger.experiment.add_figure('val/uv_traj_fig', uv_traj_fig, self.global_step)
 
