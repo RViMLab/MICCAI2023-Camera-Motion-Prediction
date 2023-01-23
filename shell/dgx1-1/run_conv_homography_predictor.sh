84e904057699ad43081c292baaa144817f43d9d2
mhubii <m.huber_1994@hotmail.de>
2023-01-23 20:45:19 +0000

    added submission files

---
+++ shell/dgx1-1/run_conv_homography_predictor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# activate conda environment
+eval "$(conda shell.bash hook)"
+conda activate torch110
+
+python /workspace/homography_imitation_learning/homography_imitation_main.py \
+  --server dgx1-1 \
+  --config config/conv_homography_predictor_phantom.yml \
+  --backbone_path endoscopy_view/resnet/34/version_0
