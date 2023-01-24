ab1b8cd7042504f36e466f2e5b99665826ecb4e7
mhubii <m.huber_1994@hotmail.de>
2023-01-24 14:19:44 +0000

    updated flags

--- shell/dgx1-1/run_conv_homography_predictor.sh
+++ shell/dgx1-1/run_conv_homography_predictor.sh
@@ -7,4 +7,4 @@ conda activate torch110
 python /workspace/homography_imitation_learning/main_homography_imitation_seq.py \
   --server dgx1-1 \
   --config config/conv_homography_predictor_phantom.yml \
-  --backbone_path ae_cai/resnet/48/25/34/version_0
+  --homography_regression ae_cai/resnet/48/25/34/version_0
