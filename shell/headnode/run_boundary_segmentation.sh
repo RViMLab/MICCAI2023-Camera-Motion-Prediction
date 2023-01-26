2f2578da836c2f28e18d8cfb294198c138b13a99
mhubii <m.huber_1994@hotmail.de>
2023-01-26 14:10:30 +0000

    moved main

--- shell/headnode/run_boundary_segmentation.sh
+++ shell/headnode/run_boundary_segmentation.sh
@@ -4,6 +4,6 @@
 eval "$(conda shell.bash hook)"
 conda activate torch110
 
-python /workspace/homography_imitation_learning/boundary_segmentation_main.py \
+python /workspace/homography_imitation_learning/main_boundary_segmentation.py \
   --server headnode \
   --config config/boundary_segmentation.yml
