915378cad79d43dd84548d9e9e0328db3147853e
mhubii <m.huber_1994@hotmail.de>
2023-01-26 21:10:29 +0000

    updated to 1.13

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker_tag=aicregistry:5000/$USER:torch110
+docker_tag=aicregistry:5000/$USER:torch113
 
-docker build . -f torch110.Dockerfile \
+docker build . -f torch113.Dockerfile \
   --tag $docker_tag \
   --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --build-arg USER=$USER \
   --network=host
