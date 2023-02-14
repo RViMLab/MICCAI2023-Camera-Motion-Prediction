090182c4d89df8a4da3e5689f7880fa242307166
mhubii <m.huber_1994@hotmail.de>
2023-02-14 11:52:30 +0000

    moved lstm

--- lightning_modules/homography_imitation/__init__.py
+++ lightning_modules/homography_imitation/__init__.py
@@ -1,5 +1,5 @@
 from .conv_homography_predictor_module import ConvHomographyPredictorModule
-from .lstm_module import (
+from .lstm_homography_predictor_module import (
     DuvLSTMModule,
     FeatureLSTMIncrementalModule,
     FeatureLSTMModule,
