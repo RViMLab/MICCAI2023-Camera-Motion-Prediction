6135ef1a3665c5da676c0f645094c625ad0ace61
mhubii <m.huber_1994@hotmail.de>
2023-02-23 14:30:09 +0000

    removed loftr

--- shell/headnode/run/conv_homography_predictor.sh
+++ shell/headnode/run/conv_homography_predictor.sh
@@ -7,4 +7,4 @@ conda activate torch110
 python /workspace/homography_imitation_learning/main_homography_imitation_seq.py \
   --server headnode \
   --config config/homography_imitation/conv_homography_predictor_phantom.yml \
-  --homography_regression ae_cai/resnet/48/25/34/version_0
+  --homography_regression ae_cai/resnet/64/25/34/version_0
